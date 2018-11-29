306880a77f9259b220403c6b8f40993ffcf401ba
renan.tomazini <renan.tomazini@LAB-DEINFO-11.deinfo.lab33>
2018-11-29 17:57:14 -0300

    Inicio

---
+++ rede_socialista/src/rede_socialista/Main.java
@@ -0,0 +1,12 @@
+package rede_socialista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Trabalhador Robson = new Trabalhador();
+		Robson.setValor(22.10);
+		Robson.Trabalhar(6);
+		
+	}
+
+}
---
+++ rede_socialista/src/rede_socialista/Trabalhador.java
@@ -0,0 +1,68 @@
+package rede_socialista;
+
+public class Trabalhador {
+	private double Valor;
+	private static double MaisValia;
+	private double Salario;
+	private double Horas;
+
+	public void Trabalhar(float Horas){
+		
+		System.out.println("trabalhando");
+		Valor = Valor * Horas;
+		System.out.println("Gerou:" + Valor);
+		Salario = Valor * MaisValia;
+		
+		System.out.println("Ganhou "+ Salario);
+	}
+	
+	
+	public Trabalhador() {
+		Trabalhador.MaisValia = 0.30;
+	}
+
+
+
+	public double getValor() {
+		return Valor;
+	}
+
+	public void setValor(double valor) {
+		Valor = valor;
+	}
+
+	public double getMaisValia() {
+		return MaisValia;
+	}
+
+	public void setMaisValia(double maisValia) {
+		MaisValia = maisValia;
+	}
+
+	public double getSalario() {
+		return Salario;
+	}
+
+	public void setSalario(double salario) {
+		Salario = salario;
+	}
+
+
+
+
+
+
+	public double getHoras() {
+		return Horas;
+	}
+
+
+
+
+
+
+	public void setHoras(double horas) {
+		Horas = horas;
+	}
+	
+}
